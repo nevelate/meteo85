d73825d2396b9e9dc2a44e2104a8d7eee3771ee8
Isfandiyor Akbarov <isfandiyorakbarov450@gmail.com>
2024-08-19 18:13:05 +0500

    initial

--- meteo85.ino
+++ meteo85.ino
@@ -1,3 +1,17 @@
+#include "GyverOLED.h"
+#include "GyverBME280.h"
+#include "GyverPower.h"
+#include "microDS3231.h"
+
+MicroDS3231 rtc;
+GyverOLED<SSH1106_128x64, OLED_NO_BUFFER> oled;
+GyverBME280 bme;
+
+#define X 128
+#define Y 64
+
+#define STANDBY_PERIOD 10000
+
 void setup() {
   // put your setup code here, to run once:
 
